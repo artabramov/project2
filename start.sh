16e143873c49aec9f06fbf5fce9069f3369eea03
Artem Abramov <notdepot@gmail.com>
2021-12-19 21:56:49 +0300

    updated

--- start.sh
+++ start.sh
@@ -1,7 +1,2 @@
-docker build -t app ./.app/
+docker build -t app ./app/
 docker-compose up -d
-
-read -t 5 -p "Wait for 5 seconds..."
-docker exec -it rabbit-2 rabbitmqctl stop_app
-docker exec -it rabbit-2 rabbitmqctl join_cluster rabbit@rabbit-1
-docker exec -it rabbit-2 rabbitmqctl start_app
